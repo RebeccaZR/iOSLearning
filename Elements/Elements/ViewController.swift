e003e4d89867dfcd1ce095056688a982ab897032
Rui Zhang <zhangrui86420@gmail.com>
2019-01-07 12:11:08 +1300

    Create a project to learn swift and UI elements.

---
+++ Elements/Elements/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Elements
+//
+//  Created by Rebecca Zhang on 2019/1/7.
+//  Copyright © 2019 Rebecca Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
