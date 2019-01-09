1736c01a8b6c977ee5bfe1a1d408f4d5f8f7b18a
Rui Zhang <zhangrui86420@gmail.com>
2019-01-09 15:42:06 +1300

    Add button exercise

--- Elements/Elements/ButtonViewController.swift
+++ Elements/Elements/ButtonViewController.swift
@@ -15,7 +15,38 @@ class ButtonViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        createDefaultButtonByCode()
+        createCustomerButton()
+    }
+    
+    private func createDefaultButtonByCode() {
+        let button = UIButton(type: UIButton.ButtonType.contactAdd)
+        button.frame = CGRect(x: 10, y: 180, width: 110, height: 30)
+        button.setTitle(" Add Button", for: UIControl.State.normal)
+        button.addTarget(self, action: #selector(defaultButtonTappedWithoutParam), for: UIControl.Event.touchUpInside)
+        self.view.addSubview(button)
+    }
+    
+    private func createCustomerButton() {
+        let customerButton = UIButton(type: UIButton.ButtonType.custom)
+        customerButton.frame = CGRect(x: 10, y: 240, width: 150, height: 30)
+        customerButton.setTitle("Customer Button", for: UIControl.State.normal)
+        customerButton.setTitleColor(UIColor.black, for: UIControl.State.normal)
+        customerButton.setImage(UIImage(named: "button"), for: UIControl.State.highlighted)
+//        customerButton.adjustsImageWhenDisabled = false
+//        customerButton.adjustsImageWhenHighlighted = false
+        customerButton.addTarget(self, action: #selector(customerButtonTapped), for: UIControl.Event.touchUpOutside)
+        self.view.addSubview(customerButton)
+    }
+    
+    @objc private func customerButtonTapped(button: UIButton) {
+        label.text = "You click customer button."
+        print("\(button.title(for: UIControl.State.highlighted)!)")
+    }
+    
+    @objc private func defaultButtonTappedWithoutParam(button: UIButton) {
+        label.text = "You click the default button that created by code."
+        print("\(button.title(for: UIControl.State.normal)!)")
     }
     
     @IBAction func buttonWasTapped(_ sender: Any) {
